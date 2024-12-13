1a4a76bd9665111b743682b6b1a544ce0dfdfcee
PaoloGoli <paolo.goli01@gmail.com>
2024-12-13 11:01:18 +0100

    changed cmake

--- src/environment_visualizer.cpp
+++ src/environment_visualizer.cpp
@@ -1,13 +1,16 @@
-#include "rclcpp/rclcpp.hpp"
-#include "gazebo_msgs/srv/spawn_entity.hpp"
-#include "gazebo_msgs/srv/delete_entity.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <gazebo_msgs/srv/spawn_entity.hpp>
+#include <gazebo_msgs/srv/delete_entity.hpp>
 #include <thread>
 #include <chrono>
 #include <vector>
 #include <string>
 
+/* 
+using namespace std;
+
 struct Box {
-    std::string name;
+    string name;
     double x, y, z;
     double size_x, size_y, size_z;
 };
@@ -19,16 +22,16 @@ public:
         delete_client_ = this->create_client<gazebo_msgs::srv::DeleteEntity>("/delete_entity");
     }
 
-    void spawnBoxes(const std::vector<Box> &boxes) {
+    void spawnBoxes(const vector<Box> &boxes) {
         for (const auto &box : boxes) {
-            auto request = std::make_shared<gazebo_msgs::srv::SpawnEntity::Request>();
+            auto request = make_shared<gazebo_msgs::srv::SpawnEntity::Request>();
             request->name = box.name;
             request->xml = createBoxSDF(box);
             request->initial_pose.position.x = box.x;
             request->initial_pose.position.y = box.y;
             request->initial_pose.position.z = box.z;
 
-            while (!spawn_client_->wait_for_service(std::chrono::seconds(1))) {
+            while (!spawn_client_->wait_for_service(chrono::seconds(1))) {
                 RCLCPP_INFO(this->get_logger(), "Waiting for /spawn_entity service...");
             }
 
@@ -41,12 +44,12 @@ public:
         }
     }
 
-    void despawnBoxes(const std::vector<std::string> &box_names) {
+    void despawnBoxes(const vector<string> &box_names) {
         for (const auto &name : box_names) {
-            auto request = std::make_shared<gazebo_msgs::srv::DeleteEntity::Request>();
+            auto request = make_shared<gazebo_msgs::srv::DeleteEntity::Request>();
             request->name = name;
 
-            while (!delete_client_->wait_for_service(std::chrono::seconds(1))) {
+            while (!delete_client_->wait_for_service(chrono::seconds(1))) {
                 RCLCPP_INFO(this->get_logger(), "Waiting for /delete_entity service...");
             }
 
@@ -63,7 +66,7 @@ private:
     rclcpp::Client<gazebo_msgs::srv::SpawnEntity>::SharedPtr spawn_client_;
     rclcpp::Client<gazebo_msgs::srv::DeleteEntity>::SharedPtr delete_client_;
 
-    std::string createBoxSDF(const Box &box) {
+    string createBoxSDF(const Box &box) {
         return R"(
             <sdf version="1.6">
               <model name=")" + box.name + R"(">
@@ -72,14 +75,14 @@ private:
                   <collision name="collision">
                     <geometry>
                       <box>
-                        <size>)" + std::to_string(box.size_x) + " " + std::to_string(box.size_y) + " " + std::to_string(box.size_z) + R"(</size>
+                        <size>)" + to_string(box.size_x) + " " + to_string(box.size_y) + " " + to_string(box.size_z) + R"(</size>
                       </box>
                     </geometry>
                   </collision>
                   <visual name="visual">
                     <geometry>
                       <box>
-                        <size>)" + std::to_string(box.size_x) + " " + std::to_string(box.size_y) + " " + std::to_string(box.size_z) + R"(</size>
+                        <size>)" + to_string(box.size_x) + " " + to_string(box.size_y) + " " + to_string(box.size_z) + R"(</size>
                       </box>
                     </geometry>
                   </visual>
@@ -93,16 +96,16 @@ private:
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
 
-    auto node = std::make_shared<BoxSpawner>();
+    auto node = make_shared<BoxSpawner>();
 
     // Define a list of boxes
-    std::vector<Box> boxes = {
+    vector<Box> boxes = {
         {"box_1", 0.0, 0.0, 0.5, 1.0, 1.0, 1.0},
         {"box_2", 2.0, 2.0, 0.5, 1.5, 1.0, 0.5},
         {"box_3", -2.0, -2.0, 0.5, 0.8, 0.8, 0.8}
     };
 
-    std::vector<std::string> box_names;
+    vector<string> box_names;
     for (const auto &box : boxes) {
         box_names.push_back(box.name);
     }
@@ -111,17 +114,22 @@ int main(int argc, char **argv) {
     node->spawnBoxes(boxes);
 
     // Wait for a few seconds
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+    this_thread::sleep_for(chrono::seconds(5));
 
     // Despawn boxes
     node->despawnBoxes(box_names);
 
     // Wait for a few seconds
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+    this_thread::sleep_for(chrono::seconds(5));
 
     // Respawn boxes
     node->spawnBoxes(boxes);
 
     rclcpp::shutdown();
+    return 0;
+}
+ */
+
+int main () {
     return 0;
 }
