6e3e92ec753740a1a6756fccacc1f8dcbbd29206
Daniele Turrini <turrini.daniele01@gmail.com>
2024-12-15 15:42:42 +0100

    refresh

--- src/TrajectoryVisualizer.cpp
+++ src/TrajectoryVisualizer.cpp
@@ -16,7 +16,7 @@ public:
     TrajectoryVisualizer()
     : Node("trajectory_visualizer")
     {
-        RCLCPP_INFO(this->get_logger(), "Starting...");
+
         publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("trajectory_marker", 10);
         timer_ = this->create_wall_timer(
             std::chrono::milliseconds(500),
@@ -59,12 +59,12 @@ private:
         }
 
         publisher_->publish(marker);
-        //RCLCPP_INFO(this->get_logger(), "Published trajectory marker");
+        RCLCPP_INFO(this->get_logger(), "Published trajectory marker");
     }
 
     vector<vector<double>> create_trajectory(const std::string& file_path)
     {
-        //RCLCPP_INFO(this->get_logger(), "Parsing Function called");
+        RCLCPP_INFO(this->get_logger(), "Parsing Function called");
         vector<vector<double>> data;
 
         ifstream file(file_path);
@@ -86,7 +86,7 @@ private:
         }
 
         file.close();
-        //RCLCPP_INFO(this->get_logger(), "File correctly parsed");
+        RCLCPP_INFO(this->get_logger(), "File correctly parsed");
 
         return data;
     }
